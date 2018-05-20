1145e053241bb34f903e56b040e1de9bee6f498a
Ivana Balazevic <ivanabalazevic1002@gmail.com>
2018-05-20 10:34:21 +0100

    Update q2.py
    fixing indentation on q2.py

--- ibalazevic/assignment1/q2.py
+++ ibalazevic/assignment1/q2.py
@@ -1,17 +1,17 @@
 class Node:
-	def __init__(self, data):
-		self.data = data
-		self.next = None
+    def __init__(self, data):
+	self.data = data
+	self.next = None
 
-def find_kth_to_last(head, k):
-	p1 = head
+    def find_kth_to_last(head, k):
+        p1 = head
 	p2 = head
 	i = 0
 	if not head:
-		return None
+	    return None
 	while(p1):
-		p1 = p1.next
-		if i > k:
-			p2 = p2.next
-		i += 1
+	    p1 = p1.next
+            if i > k:
+		p2 = p2.next
+	    i += 1
 	return p2.data
