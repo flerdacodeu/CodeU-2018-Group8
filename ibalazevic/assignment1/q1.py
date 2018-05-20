285b352367281554ce5eea4c0a6fbe7a5baef33f
Ivana Balazevic <ivanabalazevic1002@gmail.com>
2018-05-20 10:34:54 +0100

    Update q1.py
    fixing indentation on q1.py

--- ibalazevic/assignment1/q1.py
+++ ibalazevic/assignment1/q1.py
@@ -1,4 +1,4 @@
 def is_anagram(s1, s2, case_sensitive=True):
-	if not case_sensitive:
-		return sorted(s1.lower()) == sorted(s2.lower())
-	return sorted(s1) == sorted(s2)
+    if not case_sensitive:
+	return sorted(s1.lower()) == sorted(s2.lower())
+    return sorted(s1) == sorted(s2)
