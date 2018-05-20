c68d4f04f93f0f441774aeea3ae1d0b6c9ac03ed
ivana <s1656585@sms.ed.ac.uk>
2018-05-20 10:42:59 +0100

    reverting the indentation

--- ibalazevic/assignment1/q2.py
+++ ibalazevic/assignment1/q2.py
@@ -1,17 +1,17 @@
 class Node:
     def __init__(self, data):
-		self.data = data
-		self.next = None
+        self.data = data
+        self.next = None
 
     def find_kth_to_last(head, k):
-      	p1 = head
-		p2 = head
-		i = 0
-		if not head:
-	    	return None
-		while(p1):
-	    	p1 = p1.next
+        p1 = head
+        p2 = head
+        i = 0
+        if not head:
+            return None
+        while(p1):
+            p1 = p1.next
             if i > k:
-				p2 = p2.next
-	    	i += 1
-		return p2.data
+                p2 = p2.next
+            i += 1
+        return p2.data
