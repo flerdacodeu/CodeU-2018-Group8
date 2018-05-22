79bd2609b28469fc5f35e73136699de4aff08977
Cliodhna Harrison <cliodhnaharrison@gmail.com>
2018-05-22 10:55:53 +0100

    Make anagram finder able to deal with ascii, not just english alphabet

--- cliodhnaharrison/assignment1/assignment1_question1.py
+++ cliodhnaharrison/assignment1/assignment1_question1.py
@@ -1,17 +1,33 @@
+#Using Python 3
+
 import string
 
 string_one = input()
 string_two = input()
 
-alphabet = list(string.ascii_lowercase + string.ascii_uppercase)
+def anagram_finder(string_one, string_two, case_sensitive=False):
+    anagram = True
+    alphabet = list(string.printable)
 
-anagram = True
+    if case_sensitive:
+        for char in alphabet:
+            if anagram:
+                if string_one.count(char) != string_two.count(char):
+                    anagram = False
+            else:
+                return anagram
+        return anagram
 
-for char in alphabet:
-    if anagram:
-        if string_one.count(char) != string_two.count(char):
-            anagram = False
     else:
-        break
+        #Case Insensitive so making sure only lowercase letters in strings
+        string_one = string_one.lower()
+        string_two = string_two.lower()
+        for char in alphabet:
+            if anagram:
+                if string_one.count(char) != string_two.count(char):
+                    anagram = False
+            else:
+                return anagram
+        return anagram
 
-print (anagram)
+print (anagram_finder(string_one, string_two))
