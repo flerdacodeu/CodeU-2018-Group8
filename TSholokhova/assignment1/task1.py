6d9dc70dfdda226b4d9e22dbca64ab1851684c7c
TSholokhova <tanja201396@gmail.com>
2018-05-26 21:10:38 +0300

    Fixed problems in task1
    - added docstrings
    - added tests
      - test_time1 failed
    - wraped as a function

--- TSholokhova/assignment1/task1.py
+++ TSholokhova/assignment1/task1.py
@@ -1,13 +1,64 @@
-#only letters and spaces are not ignored
-s1 = input()
-s2 = input()
-words1 = ''.join([c if c.isalpha() else ' ' for c in s1]).split()
-words2 = ''.join([c if c.isalpha() else ' ' for c in s2]).split()
-sort_letters = lambda words: sorted(list(map(lambda word: sorted(word.lower()), words)))
-sort_letters_cs = lambda words: sorted(list(map(lambda word: sorted(word), words)))
-if sort_letters_cs(words1) == sort_letters_cs(words2):
-    print(s1, 'AND', s2, 'ARE CASE SENSITIVE ANAGRAMS')
-elif sort_letters(words1) == sort_letters(words2):
-    print(s1, 'AND', s2, 'ARE CASE INSENSITIVE ANAGRAMS')
-else:
-    print(s1, 'AND', s2, 'ARE NOT ANAGRAMS')
+def _normalize(string, case_sensitive=True, punctuation="none"):
+    """
+    Normalize the string according to parameters case_sensitive and punctuation.
+    :param string: initial string
+    :param case_sensitive: bool (default True)
+        False -- all letters will be converted in lowercase
+        True -- all letters will not be changed
+    :param punctuation: one of ["ignore", "none", "space"] default ("none")
+        "none" -- all punctuation marks will be considered as letters
+        "ignore" -- all punctuation marks will be removed from the string
+        "spaces" -- all punctuation marks will be converted to spaces
+    :return: normalized string
+    """
+    if not case_sensitive:
+        string = string.lower()
+    if punctuation == "spaces":
+        string = ''.join([c if c.isalpha() else ' ' for c in string])
+    elif punctuation == "ignore":
+        string = ''.join([c for c in string if c.isalpha() or c == ' '])
+    return string
+
+
+def sorted_letters(s):
+    """
+    Sort letters in each word and words in sentence s.
+    :param s: initial string, word or sentence
+    :return: list, sorted list of words from s with sorted letters
+    """
+    return sorted(map(sorted, s.split()))
+
+
+def is_anagram(s1, s2, case_sensitive=True, punctuation="none"):
+    """
+    Check whether strings are anagrams according to parameters case_sensitive and punctuation.
+    :param s1, s2: Initial strings.
+    :param case_sensitive: bool (default True)
+        False -- all letters will be converted in lowercase
+        True -- all letters will not be changed
+    :param punctuation: one of ["ignore", "none", "space"] default ("none")
+        "none" -- all punctuation marks will be considered as letters
+        "ignore" -- all punctuation marks will be removed from the string
+        "spaces" -- all punctuation marks will be converted to spaces
+    :return: bool
+        True if strings are anagrams, else False
+    """
+    s1 = _normalize(s1, case_sensitive=case_sensitive, punctuation=punctuation)
+    s2 = _normalize(s2, case_sensitive=case_sensitive, punctuation=punctuation)
+
+    if len(s1) != len(s2):
+        return False
+
+    return sorted_letters(s1) == sorted_letters(s2)
+
+#Next function implemented for compare real working time of two functions. The function can work only with words.
+from collections import Counter
+def is_anagram_linear(s1, s2):
+    """
+    Check whether two words are anagrams using Counter. Function have a linear asymptotic.
+    Words consist only letters.
+    :param s1, s2: inititial words
+    :return: bool
+        True if strings are anagrams, else False
+    """
+    return Counter(s1) == Counter(s2)
