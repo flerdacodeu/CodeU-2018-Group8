e643b58f23eed59ac9528898e13a5e5fe451d3dc
Avgustinagr <avgustinagr@gmail.com>
2018-05-30 00:34:09 +0300

    Update Task1_test.py
    Changed 1/0 to True/False

--- avgustinagr/assignment1/Task1_test.py
+++ avgustinagr/assignment1/Task1_test.py
@@ -3,12 +3,12 @@ from Task1 import*
 class test_task1(unittest.TestCase):
     """ Unit tests for Task1 """
     def test_sent_anagrams(self):
-        self.assertEqual(sent_anagrams("ellHo world","ldwor Hello",1), True)
-        self.assertEqual(sent_anagrams("ellHo !world", "ldwor! Hello", 1), True)
-        self.assertEqual(sent_anagrams("ellHo !world", "ldwor! Helll", 1), False)
-        self.assertEqual(sent_anagrams("HELLO","hello", 0), True)
+        self.assertEqual(sent_anagrams("ellHo world","ldwor Hello",True), True)
+        self.assertEqual(sent_anagrams("ellHo !world", "ldwor! Hello", True), True)
+        self.assertEqual(sent_anagrams("ellHo !world", "ldwor! Helll", True), False)
+        self.assertEqual(sent_anagrams("HELLO","hello", False), True)
 
     def test_words_anagrams(self):
-        self.assertTrue(words_anagrams("Hello","Ehllo",0))
-        self.assertFalse(words_anagrams("Hello","Ellho",1))
-        self.assertTrue(words_anagrams("Hello","Ellho",0))
+        self.assertTrue(words_anagrams("Hello","Ehllo",False))
+        self.assertFalse(words_anagrams("Hello","Ellho",True))
+        self.assertTrue(words_anagrams("Hello","Ellho",False))
