727b00f3be55a0ca2857cb94142c1c8f17d2a502
Avgustinagr <avgustinagr@gmail.com>
2018-06-11 19:22:12 +0300

    Update test_Task.py
    - Renamed the tests accordingly to the new function names
    - Added a couple of tests for KeyErrors

--- avgustinagr/assignment2/test_Task.py
+++ avgustinagr/assignment2/test_Task.py
@@ -3,7 +3,7 @@ from Task1 import*
 
 
 class TestTask(unittest.TestCase):
-    """ Unit tests for Task1 """
+    """Unit tests for Task1"""
     def setUp(self):
         self.test_tree = Tree()
         for node in 5, 2, 7, 3, 6, 1, 0:
@@ -15,13 +15,14 @@ class TestTask(unittest.TestCase):
                 0            
             """
 
-    def test_sent_anagrams(self):
-        self.assertEqual(self.test_tree.print_path_to(4), None)
+    def test_print_path_to_root(self):
+        self.assertRaises(KeyError, self.test_tree.print_path_to_root, 4)
 
-    def test_find_common_ancestor(self):
-        self.assertEquals(self.test_tree.find_common_ancestor(0, 6), 5)
-        self.assertEquals(self.test_tree.find_common_ancestor(5, 5), None)
-        self.assertEquals(self.test_tree.find_common_ancestor(0, 1), 1)
-        self.assertEquals(self.test_tree.find_common_ancestor(1, 3), 2)
-        self.assertEquals(self.test_tree.find_common_ancestor(1, 4), None)
-
+    def test_get_common_ancestor(self):
+        self.assertEquals(self.test_tree.get_common_ancestor(1, 7), 5)
+        self.assertEquals(self.test_tree.get_common_ancestor(1, 3), 2)
+        self.assertEquals(self.test_tree.get_common_ancestor(2, 2), 2)
+        self.assertEquals(self.test_tree.get_common_ancestor(0, 1), 1)
+        self.assertEquals(self.test_tree.get_common_ancestor(0, 6), 5)
+        self.assertEquals(self.test_tree.get_common_ancestor(5, 5), 5)
+        self.assertRaises(KeyError, self.test_tree.get_common_ancestor, 4, 0)
