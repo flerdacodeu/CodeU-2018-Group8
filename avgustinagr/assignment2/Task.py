a5165731a1680c9be986bee9aa663eda97edd332
Avgustinagr <avgustinagr@gmail.com>
2018-06-11 19:20:51 +0300

    Update Task.py
    - Added some more clarifying comments and gave some of the functions better names
    - Added KeyErrors
    - Rewrote the function for the second task so that it does not store additional nodes (Using the idea from the group session)

--- avgustinagr/assignment2/Task.py
+++ avgustinagr/assignment2/Task.py
@@ -1,5 +1,5 @@
 class Node:
-    """    Class Node: Each object has value and a two pointers - for left and right child.    """
+    """ Class Node: Each object has value and a two pointers - for left and right child. """
     def __init__(self, val):
         self.val = val
         self.left = None
@@ -7,59 +7,73 @@ class Node:
 
 
 class Tree:
-    """    Class Tree: Has a root of type Node    """
+    """ Class Tree: Has a root of type Node """
     def __init__(self):
         self.root = None
 
     def add(self, val):
+        """ Adds a node to the tree with the help of _add """
         if self.root is None:
             self.root = Node(val)
         else:
-            self.add_node(val, self.root)
+            self._add(val, self.root)
 
-    def add_node(self, val, node):
-        """ Adds nodes to the tree as they would be added in a BST
+    def _add(self, val, node):
+        """ Adds a node to the tree as it would be added in a BST
 
         :param val: val on the new node
         :param node: root of the tree
         """
         if val < node.val:
             if node.left:
-                self.add_node(val, node.left)
+                self._add(val, node.left)
             else:
                 node.left = Node(val)
         else:
             if node.right:
-                self.add_node(val, node.right)
+                self._add(val, node.right)
             else:
                 node.right = Node(val)
 
-    def find(self, root, val):
-        """  Finds a node in a tree by its value.
+    def _find(self, root, val):
+        """ Finds a node in a tree by its value.
 
         :param root: the root of the tree in which we are searching
         :param val: the value of the node we are searching for
-        :return: object of type Node or None if not found
+        :return: object of type Node or raises KeyError if not found
         """
         if root.val == val:
             return root
 
-        if root.right is not None:
-            n = self.find(root.right, val)
-            if n: return n
+        if root.right:
+            try:
+                n = self._find(root.right, val)
+                if n: return n
+            except KeyError:
+                pass
 
-        if root.left is not None:
-            n = self.find(root.left, val)
-            if n: return n
-        return None
+        if root.left:
+            try:
+                n = self._find(root.left, val)
+                if n: return n
+            except KeyError:
+                pass
+
+        raise KeyError("Node does not exist")
+       # return None
 
     def find_path_from_to(self, ancestor, end, path, flag_print):
-        """ Finds the path to a node from another one, which is its ancestor.
+        """ Finds the path from a node (end) to one of its ancestors (ancestor). Does not count end as a part of
+        the path.
+        ancestor must be an ancestor of end.
+        If flag_print is set to True, it prints the path reversed.
 
-        :param ancestor: object: ancestor node
+
+        :param ancestor: object: ancestor of node with value end
         :param end: object: end node
-        :param path: a stack in which we store the path
-        :param flag_print: flag, which determines whether we should print the reversed path or not
+        :param path: a stack in which we store the path (from ancestor to the parent of end)
+        :param flag_print: flag, which determines whether we should print the reversed path or not (from the parent of
+        end to ancestor)
         :return: True if found, False if not
         """
         path.append(ancestor.val)
@@ -70,62 +84,75 @@ class Tree:
                 print path[::-1]
             return True
 
-        if ancestor.left is not None:
+        if ancestor.left:
             if self.find_path_from_to(ancestor.left, end, path, flag_print):
                 return True
             path.pop()
 
-        if ancestor.right is not None:
+        if ancestor.right:
             if self.find_path_from_to(ancestor.right, end, path, flag_print):
                 return True
             path.pop()
 
         return False
 
-    def print_path_to(self, end):
+    def print_path_to_root(self, end):
         """ Prints the path from the give node to the root.
 
         :param end: value of the node
+        :return: None (the function is only meant to print the path if the node exists)
         """
-        end_node = self.find(self.root, end)
-        if end_node is None:
-            return None
+        end_node = self._find(self.root, end)
+        if end_node:
+            path = []
+            self.find_path_from_to(self.root, end_node, path, True)
 
-        path = []
-        self.find_path_from_to(self.root, end_node, path, True)
+    def _common_ancestor(self, root, _node1, _node2):
+        """ Helper function for get_common_ancestor(). (Implemented idea from the group session)
+        Takes additional parameter: root.
 
-    def find_common_ancestor(self, node1, node2):
-        """ Finds the first common ancestor of two nodes.
-        (If node1 is the parent of node2, returns node1)
-
-        :param node1: object: first node
-        :param node2: object: second node
-        :return: value of the first common ancestor (or None if the given values don`t exist in the tree)
+        :param root:  the root of the tree in which the function will be looking for _node1 / _node2
+        :param _node1: object: one of the nodes
+        :param _node2: object: the other node
+        :return: the value of the first common ancestor of _node1 and _node2
+        (Can also return None in case that one of nodes does not exist in the tree but since the function is meant to
+        be used as a helper of get_common_ancestor(), which checks for that before calling it, such a return value is
+        unlikely.)
         """
-        path1 = []
-        path2 = []
-        _node1 = self.find(self.root, node1)
-        _node2 = self.find(self.root, node2)
+        if _node1.val == root.val or _node2.val == root.val:
+            return root.val
 
-        if _node1 is None or _node2 is None:
-            return None
-
-        self.find_path_from_to(self.root, _node1, path1, False)
-        self.find_path_from_to(self.root, _node2, path2, False)
-
-        shorter_path = min(len(path1), len(path2))
-
-        if shorter_path <= 0:
-            return None
-
-        for ancestor in range (shorter_path):
-            if path1[ancestor] != path2[ancestor]:
-                    return path1[ancestor-1]
-
-        if len(path1) > shorter_path:
-            return path1[shorter_path]
-        if len(path2) > shorter_path:
-            return path2[shorter_path]
+        if root.left:
+            val1 = self._common_ancestor(root.left, _node1, _node2)
         else:
-            return path2[len(path2)-1]
-        
+            val1 = None
+
+        if root.right:
+            val2 = self._common_ancestor(root.right, _node1, _node2)
+        else:
+            val2 = None
+
+        if val1 is not None and val2 is not None:
+            return root.val
+
+        if val1 is not None:
+            return val1
+        return val2
+
+    def get_common_ancestor(self, node1, node2):
+        """ Finds the first common ancestor of two nodes.
+          (If node1 is the parent of node2, returns node1.
+          When node1 == node2 it returns the value of node1/node2)
+
+          :param node1: value of first node (not necessarily first ro be found)
+          :param node2: value of second node
+          :return: value of the first common ancestor (or raises KeyError if the given values don`t exist in the tree)
+          """
+        _node1 = self._find(self.root, node1)
+        _node2 = self._find(self.root, node2)
+
+        if not _node1 or not _node2:
+            return None
+
+        return self._common_ancestor(self.root, _node1, _node2)
+
