33a627c4fb7e2d78f3ba745869ebd321b38625d0
Avgustina Georgieva <avgustinagr@gmail.com>
2018-06-27 00:33:18 +0300

    Update Task1.py
    - removed all global variables 
    - not storing extra information about neighbours anymore

--- avgustinagr/assignment3/Task1.py
+++ avgustinagr/assignment3/Task1.py
@@ -1,5 +1,3 @@
-found = []
-
 def is_prefix(substr, dictionary):
     """Checks if substr is a prefix of any of the words in the dictionary.
     (Is case sensitive.)
@@ -36,47 +34,9 @@ def is_valid(x, y, grid):
             return False
     return True
 
-def get_neighbours(x, y, grid):
-    """ Returns an array of all the neighbours of an element in the grid.
+# found = []
 
-    :param x: the row of the element
-    :param y: the column of the element
-    :param grid: a list of lists: the grid in which the function is looking for words
-    :return: array containing its neighbours
-    """
-
-    neighbours = [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1), (x, y - 1), (x, y + 1), (x + 1, y - 1), (x + 1, y),
-                  (x + 1, y + 1)]                # indexes of all possible neighbours
-
-    for i in range(len(neighbours)):
-        if not is_valid(neighbours[i][0], neighbours[i][1], grid):
-            neighbours[i] = 0
-
-    neighbours.sort()
-
-    for i in range(len(neighbours)):            # removes all 0 elements to keep only the neighbours, which exist
-        if neighbours[i] != 0:
-            neighbours = neighbours[i:]
-            break
-    return neighbours
-
-
-def find_neighbours_of_all(grid):
-    """ Creates a dictionary with:
-    key: tuple of the element in the grid
-    value: an array of all its neighbours
-
-    :param grid: a list of lists: the grid in which the function is looking for words
-    :return: dictionary of all the tuples and their neighbours
-    """
-    all_n = {}
-    for i in range(0, len(grid)):
-        for j in range(0, len(grid[i])):
-            all_n[(i, j)] = get_neighbours(i, j, grid)
-    return all_n
-
-
-def _find_word_from(x, y, path_tuples, path, grid, dictionary):
+def _find_word_from(x, y, path_tuples, path, grid, dictionary, found):
     """ Helper function for find_words. Finds all the words from dictionary that can be formed starting at the element
     with coordinates (x, y) on the grid and appends them to the global variable found.
     Uses depth first search without keeping a list of the visited nodes so that we can traverse through them again in
@@ -89,6 +49,8 @@ def _find_word_from(x, y, path_tuples, path, grid, dictionary):
     :param path: an array of the actual letters in the word so far.
     :param grid: a list of lists: the grid in which the function is looking for words
     :param dictionary: a list of words: the dictionary with the words to which the function is comparing substr
+    :param found: a list that holds the words, which have been found so far (is being kept
+    so that the function does not print duplicates)
     """
     path_tuples.append((x, y))
     path.append(grid[x][y])
@@ -97,40 +59,46 @@ def _find_word_from(x, y, path_tuples, path, grid, dictionary):
         path_tuples.pop()
         return
     if is_word(path, dictionary) and ''.join(path) not in found:
-
+        #print ''.join(path)
         found.append(''.join(path))
 
-    for neighbour in all_neighbours[(x, y)]:
-        if neighbour not in path_tuples:
-            _find_word_from(neighbour[0], neighbour[1], path_tuples, path, grid, dictionary)
+    for i in range(x - 1, x + 2):
+        for j in range(y - 1, y + 2):
+            if is_valid(i, j, grid) and (i, j) not in path_tuples:
+                _find_word_from(i, j, path_tuples, path, grid, dictionary, found)
     path.pop()
     path_tuples.pop()
 
 
-def find_words(x, y, grid, dictionary):
+def find_words(x, y, grid, dictionary, found):
     """Main function used to find all words that can be found in the dictionary that can be formed starting at the
     element with coordinates (x, y).
     :param x: row of the element
     :param y: column of the element
     :param grid: a list of lists: the grid in which the function is looking for words
     :param dictionary: a list of words: the dictionary with the words to which the function is comparing substr
+    :param found: a list that holds the words, which have been found so far (is being kept
+    so that the function does not print duplicates)
     """
+
     if x > len(grid)-1 or x < 0 or y > len(grid[0]) or y < 0:
         print "Element out of range"
         return False
     path_tuples = []
     path = []
 
-    return _find_word_from(x, y, path_tuples, path, grid, dictionary)
+    return _find_word_from(x, y, path_tuples, path, grid, dictionary, found)
 
 
 def main(grid, dictionary):
-    """
+    """ Goes through all of the tiles, calling find_words() for each, which find every new word that has been found.
 
     :param grid: a list of lists: the grid in which the function is looking for words
     :param dictionary: a list of words: the dictionary with the words to which the function is comparing substr
-    :return:
+    :return: a list oontaining the unique words from the dictionary, which have been found in the grid
     """
+    found = []
     for i in range(0, len(grid)):
         for j in range(0, len(grid[0])):
-            find_words(i, j, grid, dictionary)
+            find_words(i, j, grid, dictionary, found)
+    return found
