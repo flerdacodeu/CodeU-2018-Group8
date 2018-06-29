ae78ed50f883e612921d9b096c193768c417b05f
ivana <s1656585@sms.ed.ac.uk>
2018-06-29 14:50:12 +0100

    adding the requested changes

--- ibalazevic/assignment4/q1.py
+++ ibalazevic/assignment4/q1.py
@@ -15,22 +15,21 @@ def count_islands(nrows, ncols, grid):
         Returns: int, number of found islands
     """
     islands = []
-    shifts = ((0, 1), (1, 0), (-1, 0), (0, -1))
-    visited_positions = set()
     for posx in range(nrows):
         for posy in range(ncols):
             curr_island = []
             pos = (posx, posy)
-            _find_islands(nrows, ncols, grid, pos, shifts, 
-                  curr_island, islands, visited_positions)
+            if grid[pos[0]][pos[1]]:
+                _find_islands(nrows, ncols, grid, pos, 
+                              curr_island, islands)
             if curr_island:
                 islands.append(curr_island)
     return len(islands)
     
 
 
-def _find_islands(nrows, ncols, grid, pos, shifts, 
-                 curr_island, islands, visited_positions):
+def _find_islands(nrows, ncols, grid, pos, 
+                 curr_island, islands):
     """
     A recursive helper method to finds all the islands in a 
     2D grid.
@@ -40,22 +39,19 @@ def _find_islands(nrows, ncols, grid, pos, shifts,
                  where True indicates land and False
                  water
         - pos - tuple of ints, current position in the grid
-        - shifts - list of tuples, the 4 possible moves moves
-                   from the current position
         - curr_island - list, a list of positions belonging
                         to the current island
         - islands - list, a list of all the islands found so far
-        - visited_positions - set of tuples, keeps track of all
-                              the True positions we've visited
     """
-    if nrows > pos[0] >= 0 and ncols > pos[1] >= 0:
-        if grid[pos[0]][pos[1]] and pos not in visited_positions:
-            curr_island.append(pos)
-            visited_positions.add(pos)
-            for shift in shifts:
+    shifts = ((0, 1), (1, 0), (-1, 0), (0, -1))
+    if grid[pos[0]][pos[1]]:
+        curr_island.append(pos)
+        grid[pos[0]][pos[1]] = False
+        for shift in shifts:
+            if (nrows > pos[0]+shift[0] >= 0 and 
+                ncols > pos[1] + shift[1] >= 0):
                 _find_islands(nrows, ncols, grid, (pos[0]+shift[0], 
-                              pos[1]+shift[1]), shifts, curr_island, 
-                              islands, visited_positions)
+                              pos[1]+shift[1]), curr_island, islands)
  
 
 
