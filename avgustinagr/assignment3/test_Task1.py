80aa3ab930bb05493b3102ef67cbc82a253df02b
Avgustina Georgieva <avgustinagr@gmail.com>
2018-07-03 18:16:38 +0300

    Update test_Task1.py

--- avgustinagr/assignment3/test_Task1.py
+++ avgustinagr/assignment3/test_Task1.py
@@ -1,22 +1,28 @@
 import unittest
-import task
+import Task1
+
 
 class TestTask(unittest.TestCase):
 
     def setUp(self):
         self.dictionary = ["CAR", "CARD", "CART", "CAT"]
         self.grid = ["AAR", "TCD"]
+        self.dictionary2 = []
+        self.grid2 = []
         self.found = []
 
     def test_example(self):
-        self.assertItemsEqual(task.main(self.grid,self.dictionary), ["CAT", "CAR", "CARD"])
+        self.assertItemsEqual(Task1.find_words(self.grid,self.dictionary), ["CAT", "CAR", "CARD"])
 
     def test_loops(self):
-        self.assertItemsEqual(task.main(["AT"], ["ATA"]), [])
+        self.assertItemsEqual(Task1.find_words(["AT"], ["ATA"]), [])
 
-    def test_find_word_with_ind_out_of_range(self):
-        self.assertEquals(task.find_words(-1, -1, self.grid, self.dictionary, self.found), False)
-        self.assertEquals(task.find_words(0, 5, self.grid, self.dictionary, self.found), False)
+    def test_empty_dict_and_grid(self):
+        self.assertItemsEqual(Task1.find_words(self.grid2, self.dictionary2), [])
+        self.assertItemsEqual(Task1.find_words(self.grid2, self.dictionary), [])
+        self.assertItemsEqual(Task1.find_words(self.grid, self.dictionary2), [])
+
+
 
 
 
