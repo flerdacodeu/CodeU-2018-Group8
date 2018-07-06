c992acd5c4f5db92c6b26375287d7c03acaa3158
TSholokhova <tanja201396@gmail.com>
2018-07-06 14:06:28 +0200

    Test update
    Added new tests
    Tests separated to little parts

--- TSholokhova/assignment4/code.py
+++ TSholokhova/assignment4/code.py
@@ -6,7 +6,12 @@ logging.basicConfig(filename="code.log", filemode='w', level=logging.DEBUG)
 class MapPainter():
     """
     Paint every land component on the map with specified color.
-    Number of resulting colors is number of land somponents on the map.
+    Number of resulting colors is number of land components on the map.
+    Attributes:
+        map: initial data, 2-dimensional array of booleans.
+        rows, cols: initial data, size of map.
+        coloring_map: 2-dimensional array of int, painted each component of the map with specified color.
+        colors: number of components
     """
     def __init__(self, rows, cols, map):
         """
@@ -17,26 +22,46 @@ class MapPainter():
         self.rows = rows
         self.cols = cols
         self.map = map
-        self.coloring_map = deepcopy(self.map)
+        self.colors = -1
+        self.coloring_map = [[]]
+        if not self._check_data():
+            return
+        self.coloring_map = [[-1] * self.cols for i in range(self.rows)]
         self.colors = 0
-        self.paint_map()
+        self._paint_map()
 
-    def paint_map(self):
+    def _check_data(self):
+        """
+        Check input.
+        :return: bool
+        False if input invalid
+        else True
+        """
+        for i in range(len(self.map)):
+            for j in range(len(self.map[i])):
+                if type(self.map[i][j]) != bool:
+                    return False
+            if len(self.map[i]) != self.cols:
+                return False
+        if len(self.map) != self.rows:
+            return False
+        return True
+
+    def _paint_map(self):
         """
         Paint every land component with specified color.
-        :return: None (self.coloring_map is changed)
         """
         for x in range(self.rows):
             for y in range(self.cols):
-                if type(self.coloring_map[x][y]) == bool:
-                    if self.coloring_map[x][y]:
+                if self.coloring_map[x][y] == -1:
+                    if self.map[x][y]:
                         self.colors += 1
-                        self.paint_component(x, y, self.colors)
+                        self._paint_component(x, y, self.colors)
                         logging.debug(self.coloring_map)
                     else:
                         self.coloring_map[x][y] = 0
 
-    def get_adjusted_tiles(self, x0, y0):
+    def _get_adjusted_tiles(self, x0, y0):
         """
        Return all adjacent tiles.
        :param x0, y0: initial tile.
@@ -50,23 +75,25 @@ class MapPainter():
             if 0 <= x_new < self.rows and 0 <= y_new < self.cols:
                 yield (x_new, y_new)
 
-    def paint_component(self, x0, y0, color):
+    def _paint_component(self, x0, y0, color):
         """
         Paint the component of the (x0, y0) tile with specified color.
         :param x0, y0: initial tile.
         :param color: specified color.
-        :return None (self.coloring_map is changed)
         """
         queue_of_tiles = queue()
         queue_of_tiles.put((x0, y0))
+        logging.debug(('start queue:', queue_of_tiles.queue, self.coloring_map))
         while not queue_of_tiles.empty():
             x_cur, y_cur = queue_of_tiles.get()
             self.coloring_map[x_cur][y_cur] = color
-            for (x, y) in self.get_adjusted_tiles(x_cur, y_cur):
-                if type(self.coloring_map[x][y]) == bool and self.coloring_map[x][y]:
+            for (x, y) in self._get_adjusted_tiles(x_cur, y_cur):
+                logging.debug((x, y, self.coloring_map[x][y], self.map))
+                if self.coloring_map[x][y] == -1 and self.map[x][y]:
                     queue_of_tiles.put((x, y))
+            logging.debug(('next queue:', queue_of_tiles.queue, self.coloring_map))
 
-    def draw_coloring_map(self):
+    def _draw_coloring_map(self):
         """
         Draw resulting map using matplotlib.
         """
@@ -80,7 +107,7 @@ def count_islands(rows, cols, map):
     :param rows: number of rows
     :param cols: number of columns
     :param map: 2-dimensional array of booleans, where false means water and true means land
-    :return: number of islands
+    :return: number of islands or -1 if input data was invalid
     """
     map_painter = MapPainter(rows, cols, map)
     return map_painter.colors
--- TSholokhova/assignment4/test.py
+++ TSholokhova/assignment4/test.py
@@ -2,8 +2,18 @@ import unittest
 from code import MapPainter, count_islands
 
 class TestCountIsands(unittest.TestCase):
+    def _map_1_island(self):
+        map = [[True, True, True],
+               [True, True, True],
+               [True, True, True]]
+        rows, cols = 3, 3
+        lands_cnt = 1
+        coloring_map = [[1, 1, 1],
+                        [1, 1, 1],
+                        [1, 1, 1]]
+        return (rows, cols, map, lands_cnt, coloring_map)
 
-    def get_map1(self):
+    def _map_non_square(self):
         map = [[False, True, False, True, False],
                [True, True, False, True, False],
                [False, False, False, True, True],
@@ -16,7 +26,7 @@ class TestCountIsands(unittest.TestCase):
                         [3, 3, 0, 0, 0]]
         return (rows, cols, map, lands_cnt, coloring_map)
 
-    def get_map2(self):
+    def _map_2_islands(self):
         map = [[True, True, True],
                [True, True, False],
                [True, False, True]]
@@ -27,7 +37,7 @@ class TestCountIsands(unittest.TestCase):
                         [1, 0, 2]]
         return (rows, cols, map, lands_cnt, coloring_map)
 
-    def get_map3(self):
+    def _map_max_islands(self):
         map = [[True, False, True],
                [False, True, False],
                [True, False, True]]
@@ -38,15 +48,106 @@ class TestCountIsands(unittest.TestCase):
                         [4, 0, 5]]
         return (rows, cols, map, lands_cnt, coloring_map)
 
-    def test_map_painter(self):
-        for get_map in [self.get_map1, self.get_map2, self.get_map3]:
-            *args, _, coloring_map = get_map()
-            self.assertEqual(MapPainter(*args).coloring_map, coloring_map)
+    def _map_0_islands(self):
+        map = [[False] * 4] * 5
+        rows, cols = 5, 4
+        lands_cnt = 0
+        coloring_map = [[0] * 4] * 5
+        return (rows, cols, map, lands_cnt, coloring_map)
 
-    def test_count_islands(self):
-        for get_map in [self.get_map1, self.get_map2, self.get_map3]:
-            *args, lands_cnt, _ = get_map()
-            self.assertEqual(count_islands(*args), lands_cnt)
+    def _map_1_row(self):
+        map = [[True, False, True, False, True]]
+        rows, cols = 1, 5
+        lands_cnt = 3
+        coloring_map = [[1, 0, 2, 0, 3]]
+        return (rows, cols, map, lands_cnt, coloring_map)
+
+
+    def _map_1_column(self):
+        map = [[True], [False], [True], [False], [True]]
+        rows, cols = 5, 1
+        lands_cnt = 3
+        coloring_map = [[1], [0], [2], [0], [3]]
+        return (rows, cols, map, lands_cnt, coloring_map)
+
+    def _map_invalid_data_type(self):
+        map = [['a'] * 3] * 6
+        rows, cols = 6, 3
+        lands_cnt = 0
+        coloring_map = [[0] * 3] * 6
+        return (rows, cols, map, lands_cnt, coloring_map)
+
+    def _map_invalid_shape(self):
+        map = [[False] * 3] * 3
+        map[1] = [False] * 4
+        rows, cols = 3, 3
+        lands_cnt = 0
+        coloring_map = [[0] * 3] * 3
+        return (rows, cols, map, lands_cnt, coloring_map)
+
+    def test_map_painter_1_island(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_1_island()
+        self.assertEqual(MapPainter(rows, cols, map).coloring_map, coloring_map)
+
+    def test_count_islands_1_island(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_1_island()
+        self.assertEqual(count_islands(rows, cols, map), lands_cnt)
+
+    def test_map_painter_non_square(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_non_square()
+        self.assertEqual(MapPainter(rows, cols, map).coloring_map, coloring_map)
+
+    def test_count_islands_non_square(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_non_square()
+        self.assertEqual(count_islands(rows, cols, map), lands_cnt)
+
+    def test_map_painter_2_islands(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_2_islands()
+        self.assertEqual(MapPainter(rows, cols, map).coloring_map, coloring_map)
+
+    def test_count_islands_2_islands(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_2_islands()
+        self.assertEqual(count_islands(rows, cols, map), lands_cnt)
+
+    def test_map_painter_max_islands(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_max_islands()
+        self.assertEqual(MapPainter(rows, cols, map).coloring_map, coloring_map)
+
+    def test_count_islands_max_islands(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_max_islands()
+        self.assertEqual(count_islands(rows, cols, map), lands_cnt)
+
+    def test_map_painter_0_islands(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_0_islands()
+        self.assertEqual(MapPainter(rows, cols, map).coloring_map, coloring_map)
+
+    def test_count_islands_0_islands(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_0_islands()
+        self.assertEqual(count_islands(rows, cols, map), lands_cnt)
+
+    def test_map_painter_1_row(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_1_row()
+        self.assertEqual(MapPainter(rows, cols, map).coloring_map, coloring_map)
+
+    def test_count_islands_1_row(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_1_row()
+        self.assertEqual(count_islands(rows, cols, map), lands_cnt)
+
+    def test_map_painter_1_column(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_1_column()
+        self.assertEqual(MapPainter(rows, cols, map).coloring_map, coloring_map)
+
+    def test_count_islands_1_column(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_1_column()
+        self.assertEqual(count_islands(rows, cols, map), lands_cnt)
+
+    def test_count_islands_invalid_data_type(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_invalid_data_type()
+        self.assertEqual(count_islands(rows, cols, map), -1)
+
+    def test_count_islands_invalid_shape(self):
+        rows, cols, map, lands_cnt, coloring_map = self._map_invalid_shape()
+        self.assertEqual(count_islands(rows, cols, map), -1)
 
 if __name__ == '__main__':
     unittest.main()
