70acf81eb3f957801b6a2615ff0a923ccabf50ea
Avgustina Georgieva <avgustinagr@gmail.com>
2018-07-09 21:50:14 +0300

    Update Task.py
    - removed extra code and visited
    - instead of visited made a copy of grid

--- avgustinagr/assignment4/Task.py
+++ avgustinagr/assignment4/Task.py
@@ -1,47 +1,40 @@
-def is_valid(x, y, grid):
+def _is_valid_land(x, y, grid):
     """ Checks whether position x, y exists in the grid and whether it is a land tile.
 
     :param x: integer for the row of the tile
     :param y: integer for the column of the tile
-    :param grid: the map of islands and water represented as a list of lists
+    :param grid: the map of islands and water represented as a list of lists with the visited land marked as False
     :return: True is the tile matches the conditions, False if not
     """
-    return (x >= 0) and (x < len(grid)) and (y >= 0) and (y < len(grid[0])) and (grid[x][y] == "T")
+    return (x >= 0) and (x < len(grid)) and (y >= 0) and (y < len(grid[0])) and grid[x][y]
 
 
-def dfs(grid, i, j, visited):
+def dfs(grid, i, j):
     """ Implements depth first search for every neighbour (excluding the diagonal neighbours) if they`re land and not
     visited.
 
-    :param grid: the map of islands and water represented as a list of lists
+    :param grid: the map of islands and water represented as a list of lists with the visited land marked as False
     :param i: integer for the row of the tile
     :param j: integer for the column of the tile
-    :param visited: a list of lists containing only True or False values. True if the tile has been visited, False if not
     """
-    visited[i][j] = True
-    if is_valid(i-1, j, grid) and not visited[i-1][j]:
-        dfs(grid, i-1, j, visited)
-    if is_valid(i+1, j, grid) and not visited[i+1][j]:
-        dfs(grid, i+1, j, visited)
-    if is_valid(i, j-1, grid) and not visited[i][j-1]:
-        dfs(grid, i, j-1, visited)
-    if is_valid(i, j+1, grid) and not visited[i][j+1]:
-        dfs(grid, i, j+1, visited)
+    grid[i][j] = False
+    for x in range(i - 1, i + 2):
+        for y in range(j - 1, j + 2):
+            if (abs((x + y) - (i + j)) == 1) and _is_valid_land(x, y, grid):
+                dfs(grid, x, y)
 
 
-def count_islands(grid):
+def count_islands(old_grid):
     """ Uses dfs() to count the connected components in the grid.
 
-    :param grid: the map of islands and water represented as a list of lists
+    :param old_grid: the map of islands and water represented as a list of lists
     :return: the number of islands from the grid
     """
-    visited = []
-    for i in range (0, len(grid)):
-        visited.append([0]*len(grid[0]))
+    grid = list(old_grid)
     count = 0
-    for i in range (0, len(grid)):
+    for i in range(0, len(grid)):
         for j in range (0, len(grid[0])):
-            if grid[i][j] == "T" and not visited[i][j]:
-                    dfs(grid, i, j, visited)
+            if old_grid[i][j] and grid[i][j]:
+                    dfs(grid, i, j)
                     count += 1
     return count
