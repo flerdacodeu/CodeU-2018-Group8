047d9f33c03b318147d4e461fdbfb53fb3da3e91
Tatjana <tatjana_cavdarova@yahoo.com>
2018-07-13 13:22:29 +0200

    Add files via upload

--- TatjanaCh/assignment3/word_search.py
+++ TatjanaCh/assignment3/word_search.py
@@ -4,24 +4,24 @@ from abc import ABCMeta, abstractmethod
 
 class Dictionary(metaclass=ABCMeta):
     """
-    Abstract Base Class, forcing derived classes to implement 'isWord' & 'isPrefix'
+    Abstract Base Class, forcing derived classes to implement 'is_word' & 'is_prefix'
     """
 
     @abstractmethod
-    def isWord(self, _str):
+    def is_word(self, _str):
         pass
 
     @abstractmethod
-    def isPrefix(self, _str):
+    def is_prefix(self, _str):
         pass
 
 
-class WordSearch():
+class WordSearch:
     def __init__(self, grid, dictionary):
         """
         
         :param grid: [2D array of chars]
-        :param dictionary: [class Dictionary] which has methods 'isWord' & 'isPrefix'
+        :param dictionary: [class Dictionary] which has methods 'is_word' & 'is_prefix'
         """
         self.grid = grid
         self.dictionary = dictionary
@@ -39,51 +39,50 @@ class WordSearch():
     def __len__(self):
         return len(self.grid)
 
-    def neighbors(self, i, j, visited):
+    def neighbors(self, i, j):
         """
         A generator that yields neighbors which are not visited
         :param i: [int]
         :param j: [int]
-        :param visited: [ 2D array of bool]
         :return: [tuple of integers] which correspond to row, column 
         """
         for ii in range(max(i - 1, 0), min(i + 2, self.n_rows)):
             for jj in range(max(j - 1, 0), min(j + 2, self.n_columns)):
-                if not visited[ii][jj]:
-                    yield ii, jj
+                yield ii, jj
+
+    def words_starting_at(self, i, j, visited, sub_word=''):
+        """
+        Given a grid of letters and a dictionary, it returns all the words from the dictionary 
+        that can be formed starting from char (i, j) in the grid (without repetition)
+        :param i: [int]
+        :param j: [int]
+        :param sub_word: [ str ]
+        :param visited: [ 2D array of bool] True and False denote visited and not visited letter, respectively
+        :return: [set]  of words
+        """
+        words = set()
+        if visited[i][j]:
+            return words
+
+        sub_word += self[i, j]
+        visited[i][j] = True
+
+        if self.dictionary.is_word(sub_word):
+            words.add(sub_word)
+
+        if self.dictionary.is_prefix(sub_word):
+            for ii, jj in self.neighbors(i, j):
+                words.update(self.words_starting_at(i=ii, j=jj, visited=visited, sub_word=sub_word[:]))
+        visited[i][j] = False
+        return words
 
 
-def wordsStartingAt(grid, i, j, visited, sub_word=''):
-    """
-    Given a grid of letters and a dictionary, it returns all the words from the dictionary 
-    that can be formed starting from char (i, j) in the grid (without repetition)
-    :param grid: [class WordSearch] see above
-    :param i: [int]
-    :param j: [int]
-    :param sub_word: [ str ]
-    :param visited: [ 2D array of bool] True and False denote visited and not visited letter, respectively
-    :return: [set]  of words
-    """
-    words = set()
-    sub_word += grid[i, j]
-    visited[i][j] = True
-
-    if grid.dictionary.isWord(sub_word):
-        words.add(sub_word)
-
-    if grid.dictionary.isPrefix(sub_word):
-        for ii, jj in grid.neighbors(i, j, visited):
-            words.update(wordsStartingAt(grid, i=ii, j=jj, visited=visited, sub_word=sub_word[:]))
-    visited[i][j] = False
-    return words
-
-
-def wordSearch(grid_chars, dictionary):
+def word_search(grid_chars, dictionary):
     """
     Given a grid of letters and a dictionary, it returns all the words from the dictionary 
     that can be formed in the grid (without repetition)
     :param grid_chars: [2D array of chars]
-    :param dictionary: [class Dictionary] which has methods 'isWord' & 'isPrefix'
+    :param dictionary: [class Dictionary] which has methods 'is_word' & 'is_prefix'
     :return: set of words from dictionary, found by traversing grid
     """
     words = set()
@@ -91,5 +90,5 @@ def wordSearch(grid_chars, dictionary):
     visited = [[False for _ in range(grid.n_columns)] for _ in range(grid.n_rows)]
     for i in range(grid.n_rows):
         for j in range(grid.n_columns):
-            words.update(wordsStartingAt(grid, i, j, visited))
+            words.update(grid.words_starting_at(i, j, visited))
     return words
--- TatjanaCh/assignment3/word_search_test.py
+++ TatjanaCh/assignment3/word_search_test.py
@@ -8,12 +8,12 @@ from word_search import *
 class MySimpleDictionary(Dictionary):
     """
     A dictionary that assumes storage of only few words. 
-    It is used for testing and 'isPrefix' is not optimized.
+    It is used for testing and 'is_prefix' is not optimized.
     """
     def __init__(self, words):
         self.words = set(words)
 
-    def isWord(self, _str):
+    def is_word(self, _str):
         """
         Returns whether the given string is a valid word. Not case sensitive
         :param _str: [string]
@@ -21,7 +21,7 @@ class MySimpleDictionary(Dictionary):
         """
         return True if _str in self.words else False
 
-    def isPrefix(self, _str):
+    def is_prefix(self, _str):
         """
         Returns whether the given string is a prefix of at least one word in the dictionary.
         If _str is empty string, returns False. Not case sensitive.
@@ -39,7 +39,7 @@ class EnglishDictionary(Dictionary):
     A wrapper of the library nltk.corpus
     """
 
-    def isWord(self, _str):
+    def is_word(self, _str):
         """
         Returns whether the given string is a valid word. Not case sensitive
         :param _str: [string]
@@ -47,7 +47,7 @@ class EnglishDictionary(Dictionary):
         """
         return True if wordnet.synsets(_str) else False
 
-    def isPrefix(self, _str):
+    def is_prefix(self, _str):
         """
         Returns whether the given string is a prefix of at least one word in the dictionary.
         If _str is empty string, returns False. Not case sensitive
@@ -60,28 +60,28 @@ class EnglishDictionary(Dictionary):
 class TestsWordSearch(unittest.TestCase):
     def testEmptyDictionary(self):
         _dict = MySimpleDictionary([])
-        self.assertEqual(len(wordSearch(['a', 'b'], _dict)), 0)
+        self.assertEqual(len(word_search(['a', 'b'], _dict)), 0)
 
     def testEmptyGrid(self):
         _dict = MySimpleDictionary(['flower'])
-        self.assertEqual(len(wordSearch([], _dict)), 0)
+        self.assertEqual(len(word_search([], _dict)), 0)
 
     def testSimpleDictionary(self):
         grid_chars = [['a', 'a', 'r'], ['t', 'c', 'd']]
-        simpleDictionary = MySimpleDictionary(['car', 'card', 'cart', 'cat'])
-        groundTruthDict = set(['cat', 'car', 'card'])
-        self.assertEqual(wordSearch(grid_chars, simpleDictionary), groundTruthDict)
+        simple_dictionary = MySimpleDictionary(['car', 'card', 'cart', 'cat'])
+        ground_truth = set(['cat', 'car', 'card'])
+        self.assertEqual(word_search(grid_chars, simple_dictionary), ground_truth)
 
     def testExample(self):
         grid_chars = [['w', 'm', 'l'],
                       ['l', 'i', 'a'],
                       ['e', 'k', 'c']]
-        Dictionary = EnglishDictionary()
-        groundTruthDict = set(['mile', 'i', 'aim', 'iw', 'lack', 'ia', 'milk', 'ca', 'mil', 'mak', 'el', 'lam', 'c',
-                               'wi', 'kc', 'ai', 'la', 'ma', 'ml', 'cam', 'k', 'lek', 'ie', 'li', 'mac', 'ci', 'ka',
-                               'mi', 'a', 'make', 'w', 'l', 'al', 'ki', 'le', 'm', 'lei', 'il', 'lac', 'cim', 'elk',
-                               'cia', 'lm', 'elm', 'ic', 'ak', 'ali', 'mack', 'ac', 'ail', 'lie', 'am', 'e', 'ilk'])
-        self.assertEqual(wordSearch(grid_chars, Dictionary), groundTruthDict)
+        dictionary = EnglishDictionary()
+        ground_truth = set(['mile', 'i', 'aim', 'iw', 'lack', 'ia', 'milk', 'ca', 'mil', 'mak', 'el', 'lam', 'c',
+                            'wi', 'kc', 'ai', 'la', 'ma', 'ml', 'cam', 'k', 'lek', 'ie', 'li', 'mac', 'ci', 'ka',
+                            'mi', 'a', 'make', 'w', 'l', 'al', 'ki', 'le', 'm', 'lei', 'il', 'lac', 'cim', 'elk',
+                            'cia', 'lm', 'elm', 'ic', 'ak', 'ali', 'mack', 'ac', 'ail', 'lie', 'am', 'e', 'ilk'])
+        self.assertEqual(word_search(grid_chars, dictionary), ground_truth)
 
 
 if __name__ == '__main__':
