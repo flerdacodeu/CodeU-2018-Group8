0fc9dd94f691e0f298d277efd98ccd0cbab1b748
TSholokhova <tanja201396@gmail.com>
2018-07-13 17:49:27 +0200

    Challenge 1
    ** def build_graph: rewrited, now it is more compact.
    ** def all_alphabets: new function, challenge 1 (I think it should have more tests, I added only one yet.)

--- group/assignment5/q1.py
+++ group/assignment5/q1.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import unittest
+from copy import deepcopy
 
 def build_graph(dictionary):
     """
@@ -19,28 +20,16 @@ def build_graph(dictionary):
             characters that have outgoing edges
             from the current character)
     """
-    level = 0
-    graph = {}
-    change = True
-
-    while(change):
-        change = False
-        curr_letter = ""
-        curr_prefix = ""
-        for word in dictionary:
-            if level < len(word):
-                change = True
-                if word[level] not in graph:
-                    graph[word[level]] = {"outgoing":set(), "incoming":set()}
-                if word[level] != curr_letter and word[:level] == curr_prefix:
-                    if curr_letter:
-                        if curr_letter not in graph:
-                            graph[curr_letter] = {"outgoing":set(), "incoming":set()}
-                        graph[curr_letter]["outgoing"].add(word[level])
-                        graph[word[level]]["incoming"].add(curr_letter)
-                curr_letter = word[level]
-                curr_prefix = word[:level]
-        level += 1
+    characters = set()
+    for word in dictionary:
+        characters = characters.union(word)
+    graph = {char : {"outgoing":set(), "incoming":set()} for char in characters}
+    for i in range(0, len(dictionary) - 1):
+        for c1, c2 in zip(dictionary[i], dictionary[i+1]):
+            if c1 != c2:
+                graph[c1]["outgoing"].add(c2)
+                graph[c2]["incoming"].add(c1)
+                break
     return graph
 
 def unknown_alphabet(dictionary):
@@ -72,6 +61,42 @@ def unknown_alphabet(dictionary):
     return alphabet
 
 
+#challenge1
+def all_alphabets(dictionary):
+    """
+        Extracts all possible alphabets (ordered list of characters)
+        from a lexicographically ordered dictionary of words.
+            - dictionary: list of strings, all the
+                          words in the dictionary
+            Returns:
+                - all_alphabets: list of ordered alphabets list of strings
+        """
+    def _brute_force(starting_nodes, graph):
+        for curr_node in starting_nodes:
+            tmp_graph = deepcopy(graph)
+            tmp_starting_nodes = deepcopy(starting_nodes)
+            tmp_starting_nodes.remove(curr_node)
+            alphabet.append(curr_node)
+            for node in tmp_graph[curr_node]["outgoing"].copy():
+                tmp_graph[curr_node]["outgoing"].remove(node)
+                tmp_graph[node]["incoming"].remove(curr_node)
+                if not tmp_graph[node]["incoming"]:
+                    tmp_starting_nodes.append(node)
+            _brute_force(tmp_starting_nodes, tmp_graph)
+            alphabet.pop()
+        if not starting_nodes:
+            result_alphabets.append(deepcopy(alphabet))
+            for node in graph:
+                if graph[node]["incoming"] or graph[node]["outgoing"]:
+                    raise ValueError("Dictionary is inconsistent!")
+
+    dictionary = map(lambda x: x.decode("utf8"), dictionary)
+    graph = build_graph(dictionary)
+    starting_nodes = [node for node, edges in graph.items() if not edges["incoming"]]
+    alphabet = []
+    result_alphabets = []
+    _brute_force(starting_nodes, graph)
+    return result_alphabets
 
 class AlphabetTest(unittest.TestCase):
 
@@ -97,6 +122,10 @@ class AlphabetTest(unittest.TestCase):
     def test_nonascii_chars(self):
         self.assertEqual(unknown_alphabet(["älp", "ärt", "ärm", "rat", "cat", "car"]),
                     map(lambda x: x.decode("utf8"), ["t", "m", "p", "l", "ä", "r", "c", "a"]))
+
+class AllAlphabetTest(unittest.TestCase):
+    def test_base(self):
+        self.assertEqual(all_alphabets(["art", "rat", "cat", "car"]), [list('atrc'), list('tarc')])
 
 if __name__ == "__main__":
     unittest.main()
