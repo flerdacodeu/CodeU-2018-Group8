2ea5948baa0c81361ae62cea917e6b3177b5e47b
Avgustina Georgieva <avgustinagr@gmail.com>
2018-07-15 00:40:06 +0300

    Update Task.py

--- avgustinagr/assignment4/Task.py
+++ avgustinagr/assignment4/Task.py
@@ -9,7 +9,7 @@ def _is_valid_land(x, y, grid):
     return (x >= 0) and (x < len(grid)) and (y >= 0) and (y < len(grid[0])) and grid[x][y]
 
 
-def dfs(grid, i, j):
+def _dfs(grid, i, j):
     """ Implements depth first search for every neighbour (excluding the diagonal neighbours) if they`re land and not
     visited.
 
@@ -21,20 +21,20 @@ def dfs(grid, i, j):
     for x in range(i - 1, i + 2):
         for y in range(j - 1, j + 2):
             if (abs((x + y) - (i + j)) == 1) and _is_valid_land(x, y, grid):
-                dfs(grid, x, y)
+                _dfs(grid, x, y)
 
 
-def count_islands(old_grid):
-    """ Uses dfs() to count the connected components in the grid.
+def count_islands(grid):
+    """ Uses _dfs() to count the connected components in the grid.
 
-    :param old_grid: the map of islands and water represented as a list of lists
+    :param grid: the map of islands and water represented as a list of lists
     :return: the number of islands from the grid
     """
-    grid = list(old_grid)
+    grid_copy = list(grid)
     count = 0
-    for i in range(0, len(grid)):
-        for j in range (0, len(grid[0])):
-            if old_grid[i][j] and grid[i][j]:
-                    dfs(grid, i, j)
+    for i in range(0, len(grid_copy)):
+        for j in range (0, len(grid_copy[0])):
+            if grid[i][j] and grid_copy[i][j]:
+                    _dfs(grid_copy, i, j)
                     count += 1
     return count
