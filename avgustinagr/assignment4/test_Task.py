2a801fa166350b217cba9892f3f79e9c537e751e
Avgustina Georgieva <avgustinagr@gmail.com>
2018-07-15 00:40:32 +0300

    Update test_Task.py

--- avgustinagr/assignment4/test_Task.py
+++ avgustinagr/assignment4/test_Task.py
@@ -4,45 +4,43 @@ import Task
 
 class TestTask(unittest.TestCase):
 
-    def setUp(self):
+    def test_count_islands_general_case(self):
         self.grid = [[False, True, False, True], [True, True, False, False], [False, False, True, False],
                      [False, False, True, False]]
         self.grid1 = [[True, True], [True, False], [False, True]]
-        self.grid2 = [[True]]
-        self.grid3 = [[True, True, True], [True, True, True]]
-        self.grid4 = [[False]]
-        self.grid5 = [[False, False, False], [False, False, False]]
-        self.grid6 = []
-        self.grid7 = [[True, True, False, False, False], [False, True, False, False, True],
+        self.grid2 = [[True, True, False, False, False], [False, True, False, False, True],
                       [True, False, False, True, True],
                       [False, False, False, False, False], [True, False, True, False, True]]
-        self.grid8 = [[True, False], [False, True]]
-        self.grid9 = [[False, True], [True, False]]
-        self.grid10 = [[True, False], [True, False]]
-
-    def test_count_islands_general_case(self):
         self.assertEquals(Task.count_islands(self.grid), 3)
         self.assertEquals(Task.count_islands(self.grid1), 2)
-        self.assertEquals(Task.count_islands(self.grid7), 6)
+        self.assertEquals(Task.count_islands(self.grid2), 6)
 
     def test_one_T_tile(self):
-        self.assertEquals(Task.count_islands(self.grid2), 1)
+        self.grid = [[True]]
+        self.assertEquals(Task.count_islands(self.grid), 1)
 
     def test_one_F_tile(self):
-        self.assertEquals(Task.count_islands(self.grid4), 0)
+        self.grid = [[False]]
+        self.assertEquals(Task.count_islands(self.grid), 0)
 
     def test_all_tiles_T(self):
-        self.assertEquals(Task.count_islands(self.grid3), 1)
+        self.grid = [[True, True, True], [True, True, True]]
+        self.assertEquals(Task.count_islands(self.grid), 1)
 
     def test_all_tiles_F(self):
-        self.assertEquals(Task.count_islands(self.grid5), 0)
+        self.grid = [[False, False, False], [False, False, False]]
+        self.assertEquals(Task.count_islands(self.grid), 0)
 
     def test_empty_grid_returns_0(self):
-        self.assertEquals(Task.count_islands(self.grid6), 0)
+        self.grid = []
+        self.assertEquals(Task.count_islands(self.grid), 0)
 
     def test_four_tiles_2_Ts_diagonally(self):
-        self.assertEquals(Task.count_islands(self.grid8), 2)
-        self.assertEquals(Task.count_islands(self.grid9), 2)
+        self.grid = [[True, False], [False, True]]
+        self.grid1 = [[False, True], [True, False]]
+        self.assertEquals(Task.count_islands(self.grid), 2)
+        self.assertEquals(Task.count_islands(self.grid1), 2)
 
     def test_four_tiles_2_Ts_vertically(self):
-        self.assertEquals(Task.count_islands(self.grid10), 1)
+        self.grid = [[True, False], [True, False]]
+        self.assertEquals(Task.count_islands(self.grid), 1)
