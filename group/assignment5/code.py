3cf981ed24597800266691a5232a4049b4afd471
TSholokhova <tanja201396@gmail.com>
2018-07-16 13:15:16 +0200

    Refactored and added new tests

--- group/assignment5/code.py
+++ group/assignment5/code.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-import unittest
 from copy import deepcopy
 
-#main problem
-def build_graph(dictionary):
+
+# main problem
+def _build_graph(dictionary):
     """
     Builds a directed graph of characters from a 
     lexicographically ordered dictionary of words.
@@ -22,16 +22,18 @@ def build_graph(dictionary):
             from the current character)
     """
     characters = {character for word in dictionary for character in word}
-    graph = {char : {"outgoing":set(), "incoming":set()} for char in characters}
+    graph = {char: {"outgoing": set(), "incoming": set()}
+             for char in characters}
     for i in range(0, len(dictionary) - 1):
-        for c1, c2 in zip(dictionary[i], dictionary[i+1]):
+        for c1, c2 in zip(dictionary[i], dictionary[i + 1]):
             if c1 != c2:
                 graph[c1]["outgoing"].add(c2)
                 graph[c2]["incoming"].add(c1)
                 break
     return graph
 
-def unknown_alphabet(dictionary):
+
+def get_alphabet(dictionary):
     """
     Extracts an alphabet (ordered list of characters)
     from a lexicographically ordered dictionary of words.
@@ -41,11 +43,11 @@ def unknown_alphabet(dictionary):
             - alphabet: list of strings, an ordered
                         list of characters
     """
-    graph = build_graph(dictionary)
+    graph = _build_graph(dictionary)
     starting_nodes = [node for node, edges in graph.items() if not edges["incoming"]]
 
     alphabet = []
-    while(starting_nodes):
+    while starting_nodes:
         curr_node = starting_nodes.pop()
         alphabet.append(curr_node)
         for node in graph[curr_node]["outgoing"].copy():
@@ -59,8 +61,8 @@ def unknown_alphabet(dictionary):
     return alphabet
 
 
-#challenge1
-def all_alphabets(dictionary):
+# challenge1
+def get_all_alphabets(dictionary):
     """
         Extracts all possible alphabets (ordered list of characters)
         from a lexicographically ordered dictionary of words.
@@ -69,6 +71,7 @@ def all_alphabets(dictionary):
             Returns:
                 - all_alphabets: list of ordered alphabets list of strings
         """
+
     def _brute_force(starting_nodes, graph):
         for curr_node in starting_nodes:
             tmp_graph = deepcopy(graph)
@@ -88,46 +91,8 @@ def all_alphabets(dictionary):
                 if graph[node]["incoming"] or graph[node]["outgoing"]:
                     raise ValueError("Dictionary is inconsistent!")
 
-    graph = build_graph(dictionary)
-    starting_nodes = [node for node, edges in graph.items() if not edges["incoming"]]
+    main_graph = _build_graph(dictionary)
+    main_starting_nodes = [node for node, edges in main_graph.items()
+                           if not edges["incoming"]]
     alphabet = []
-    return list(_brute_force(starting_nodes, graph))
-
-class AlphabetTest(unittest.TestCase):
-
-    def test_empty(self):
-        self.assertEqual(unknown_alphabet([]), [])
-
-    def test_empty_string(self):
-        self.assertEqual(unknown_alphabet([""]), [])
-
-    def test_one_word(self):
-        self.assertTrue(tuple(unknown_alphabet(["abc"])) in {tuple("abc"),
-                                                             tuple("acb"),
-                                                             tuple("bac"),
-                                                             tuple("bca"),
-                                                             tuple("cab"),
-                                                             tuple("cba"),
-                                                             })
-
-    def test_base_example(self):
-        self.assertTrue(tuple(unknown_alphabet(["art", "rat", "cat", "car"])) in
-                        {tuple("tarc"), tuple("atrc")})
-
-    def test_more_complicated_example(self):
-        self.assertTrue(unknown_alphabet(["alp", "art", "arm", "rat", "cat", "car"]) in
-                         all_alphabets(["alp", "art", "arm", "rat", "cat", "car"]))
-
-    def test_cycle_raises_valueerror(self):
-        self.assertRaises(ValueError, unknown_alphabet, ["art", "rat", "cat", "car", "rr", "ra"])
-
-    def test_nonascii_chars(self):
-        self.assertCountEqual(unknown_alphabet(["älp", "ärt", "ärm", "rat", "cat", "car"]),
-                    map(lambda x: x, ["t", "m", "p", "l", "ä", "r", "c", "a"]))
-
-class AllAlphabetTest(unittest.TestCase):
-    def test_all_alphabets_base(self):
-        self.assertCountEqual(all_alphabets(["art", "rat", "cat", "car"]), [list('atrc'), list('tarc')])
-
-if __name__ == "__main__":
-    unittest.main()
+    return list(_brute_force(main_starting_nodes, main_graph))
---
+++ group/assignment5/tests.py
@@ -0,0 +1,80 @@
+import unittest
+from code import get_alphabet, get_all_alphabets
+
+
+class AlphabetTest(unittest.TestCase):
+
+    def test_get_alphabet_empty(self):
+        self.assertEqual(get_alphabet([]), [])
+
+    def test_get_alphabet_one_empty_word(self):
+        self.assertEqual(get_alphabet(['']), [])
+
+    def test_get_alphabet_all_empty_words(self):
+        self.assertEqual(get_alphabet(['', '', '']), [])
+
+    def test_get_alphabet_some_empty_words(self):
+        self.assertEqual(get_alphabet(['', '', 'A']), ['A'])
+
+    def test_get_alphabet_one_word(self):
+        self.assertTrue(tuple(get_alphabet(["abc"])) in
+                        {tuple("abc"), tuple("acb"), tuple("bac"),
+                         tuple("bca"), tuple("cab"), tuple("cba")})
+
+    def test_get_alphabet_simple(self):
+        self.assertTrue(tuple(get_alphabet(["art", "rat", "cat", "car"])) in
+                        {tuple("tarc"), tuple("atrc")})
+
+    def test_get_alphabet_cycle_raises_value_error(self):
+        self.assertRaises(ValueError, get_alphabet,
+                          ["art", "rat", "cat", "car", "rr", "ra"])
+
+    def test_get_alphabet_identical_words(self):
+        self.assertTrue(tuple(get_alphabet(['ART', 'ART', 'ART'])) in
+                        {tuple('ART'), tuple('ATR'), tuple('RAT'),
+                         tuple('RTA'), tuple('TAR'), tuple('TRA')})
+
+
+class AllAlphabetsTest(unittest.TestCase):
+
+    def test_get_all_alphabets_empty(self):
+        self.assertEqual(get_all_alphabets([]), [[]])
+
+    def test_get_all_alphabets_one_empty_word(self):
+        self.assertEqual(get_all_alphabets(['']), [[]])
+
+    def test_get_all_alphabets_all_empty_words(self):
+        self.assertEqual(get_all_alphabets(['', '', '']), [[]])
+
+    def test_get_all_alphabets_some_empty_words(self):
+        self.assertEqual(get_all_alphabets(['', '', 'A']), [['A']])
+
+    def test_get_all_alphabets_one_word(self):
+        self.assertCountEqual(get_all_alphabets(["abc"]),
+                              [list("abc"), list("acb"), list("bac"),
+                               list("bca"), list("cab"), list("cba")])
+
+    def test_get_all_alphabets_simple(self):
+        self.assertCountEqual(get_all_alphabets(["art", "rat", "cat", "car"]),
+                              [list("tarc"), list("atrc")])
+
+    def test_get_all_alphabets_cycle_raises_value_error(self):
+        self.assertRaises(ValueError, get_all_alphabets,
+                          ["art", "rat", "cat", "car", "rr", "ra"])
+
+    def test_get_all_alphabets_identical_words(self):
+        self.assertCountEqual(get_all_alphabets(['ART', 'ART', 'ART']),
+                              [list('ART'), list('ATR'), list('RAT'),
+                               list('RTA'), list('TAR'), list('TRA')])
+
+    def test_alphabet_functions_complicated(self):
+        self.assertTrue(get_alphabet(["alp", "art", "arm", "rat", "cat", "car"]) in
+                        get_all_alphabets(["alp", "art", "arm", "rat", "cat", "car"]))
+
+    def test_alphabet_functions_any_character(self):
+        self.assertTrue(get_alphabet(["älp", "ärt", "ärm", "rat", "cat", "car"]) in
+                        get_all_alphabets(["älp", "ärt", "ärm", "rat", "cat", "car"]))
+
+
+if __name__ == "__main__":
+    unittest.main()
