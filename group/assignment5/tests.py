734a15781ffba60577de8c3c34c89e3d78cec631
Avgustina Georgieva <avgustinagr@gmail.com>
2018-07-24 15:28:31 +0300

    Update tests.py
    changed some to regex

--- group/assignment5/tests.py
+++ group/assignment5/tests.py
@@ -1,6 +1,6 @@
 import unittest
-from code import get_alphabet
 
+from code import get_alphabet, get_all_alphabets
 
 class AlphabetTest(unittest.TestCase):
     def test_get_alphabet_empty(self):
@@ -27,6 +27,51 @@ class AlphabetTest(unittest.TestCase):
 
     def test_get_alphabet_identical_words(self):
         self.assertRegexpMatches(''.join(get_alphabet(['ART', 'ART', 'ART'])), r'^(?:([ART])(?!.*\1)){3}$')
+
+    def test_alphabet_functions_any_character(self):
+        self.assertRegexpMatches(''.join(get_alphabet(["äl", "är", "lä", "🐨"])), r'^äl(?:([🐨r])(?!.*\1)){2}$')
+
+class AllAlphabetsTest(unittest.TestCase):
+
+    def test_get_all_alphabets_empty(self):
+        self.assertEqual(get_all_alphabets([]), [[]])
+
+    def test_get_all_alphabets_one_empty_word(self):
+        self.assertEqual(get_all_alphabets([""]), [[]])
+
+    def test_get_all_alphabets_all_empty_words(self):
+        self.assertEqual(get_all_alphabets(["", "", ""]), [[]])
+
+    def test_get_all_alphabets_some_empty_words(self):
+        self.assertEqual(get_all_alphabets(["", "", "A"]), [["A"]])
+
+    def test_get_all_alphabets_one_word(self):
+        self.assertCountEqual(get_all_alphabets(["abc"]),
+                              [list("abc"), list("acb"), list("bac"),
+                               list("bca"), list("cab"), list("cba")])
+
+    def test_get_all_alphabets_simple(self):
+        self.assertCountEqual(get_all_alphabets(["art", "rat", "cat", "car"]),
+                              [list("tarc"), list("atrc")])
+
+    def test_get_all_alphabets_cycle_raises_value_error(self):
+        self.assertRaises(ValueError, get_all_alphabets,
+                          ["art", "rat", "cat", "car", "rr", "ra"])
+
+    def test_get_all_alphabets_identical_words(self):
+        self.assertCountEqual(get_all_alphabets(["ART", "ART", "ART"]),
+                              [list("ART"), list("ATR"), list("RAT"),
+                               list("RTA"), list("TAR"), list("TRA")])
+
+    def test_alphabet_functions_complicated(self):
+        self.assertTrue(
+            get_alphabet(["alp", "art", "arm", "rat", "cat", "car"]),
+            get_all_alphabets(["alp", "art", "arm", "rat", "cat", "car"]))
+
+    def test_alphabet_functions_any_character(self):
+        self.assertIn(
+            get_alphabet(["älp", "ärt", "ärm", "rat", "cat", "car"]),
+            get_all_alphabets(["älp", "ärt", "ärm", "rat", "cat", "car"]))
 
 
 if __name__ == "__main__":
