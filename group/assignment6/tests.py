90873a80ac0f2841e9cdc4e03e1a0aa0b3949a57
Aliia <26248907+aliiae@users.noreply.github.com>
2018-07-26 00:45:05 +0200

    added some tests

--- group/assignment6/tests.py
+++ group/assignment6/tests.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import unittest
 
 from code import Parking
@@ -5,25 +6,36 @@ from code import Parking
 
 class ParkingTest(unittest.TestCase):
     def test_empty_parking(self):
-        self.assertEqual(Parking([], []).get_moves(), [])
+        self.assertListEqual(Parking([], []).get_moves(), [])
 
     def test_parking_size_1(self):
-        self.assertEqual(Parking([0], [0]).get_moves(), [])
+        self.assertListEqual(Parking([0], [0]).get_moves(), [])
 
     def test_parking_size_2(self):
-        self.assertEqual(Parking([0, 1], [1, 0]).get_moves(), [(1, 0)])
+        self.assertListEqual(Parking([0, 1], [1, 0]).get_moves(), [(1, 0)])
 
     def test_parking_equal_start_end_states(self):
-        self.assertEqual(Parking([0, 1, 2, 3], [0, 1, 2, 3]).get_moves(), [])
+        self.assertListEqual(Parking([0, 1, 2, 3], [0, 1, 2, 3]).get_moves(),
+                             [])
 
     def test_parking_basic(self):
-        self.assertEqual(Parking([1, 2, 0, 3], [3, 1, 2, 0]).get_moves(), [(2, 2), (1, 1), (3, 0)])
+        self.assertListEqual(Parking([1, 2, 0, 3], [3, 1, 2, 0]).get_moves(),
+                             [(2, 2), (1, 1), (3, 0)])
 
     def test_incorrect_data_types(self):
         self.assertRaises(ValueError, Parking, {1, 2, 3, 0}, {1, 2, 3, 0})
+        self.assertRaises(ValueError, Parking, None, None)
 
-    def test_incorrect_data_values(self):
+    def test_incorrect_data_values_no_empty_slot(self):
         self.assertRaises(ValueError, Parking, [1, 2, 3, 4], [1, 2, 3, 0])
+        self.assertRaises(ValueError, Parking, [1, 2, 3, 0], [1, 2, 3, 4])
+
+    @unittest.skip  # TODO: should we check this?
+    def test_incorrect_data_values_two_empty_slots(self):
+        self.assertRaises(ValueError, Parking, [1, 2, 0, 0], [0, 0, 1, 2])
+
+    def test_incorrect_data_values_diff_cars_in_start_end(self):
+        self.assertRaises(ValueError, Parking, [0, 1, 2, 3], [0, 1, 2, 4])
 
     def test_incorrect_data_sizes(self):
         self.assertRaises(ValueError, Parking, [1, 2, 0], [1, 2, 3, 0])
