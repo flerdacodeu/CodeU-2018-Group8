91b828cd6164e2aac224a1ed9fed55167c0aaa6b
Aliia <26248907+aliiae@users.noreply.github.com>
2018-08-05 16:22:30 +0100

    backtracking solution for ch3

--- group/assignment6/code.py
+++ group/assignment6/code.py
@@ -1,31 +1,17 @@
 # -*- coding: utf-8 -*-
-"""Computes a sequence of moves from the start state to the end state.
-
-Fewer moves:
-    We can't decrease the count of incorrectly parked cars by more
-    than 1 for one movement.
-    If initially the empty slot is in the right place we can't decrease
-    count of incorrect cars, so we shouldn't increase it
-    (we can move any incorrect car to empty place).
-    If the empty slot is not in the right place we can decrease
-    count of incorrect cars by 1 (we can move correct car to empty place).
-    Number of times when the empty slot will be on its right place equals
-    to the number of cycles in the permutation.
-    So, minimal count of movements = SUM (l_i + 1), where i from 1 to N,
-    N is the count of permutation's cycles, l_i is a length of i-th cycle.
-"""
+"""Computes a sequence of moves from the start state to the end state."""
 
 import copy
 import logging
-from typing import List, Tuple, Set, Dict, Hashable
+from typing import List, Set, Dict, Hashable, NamedTuple, Generator, Optional
 
 logging.basicConfig(filename="parking.log", filemode="w", level=logging.DEBUG)
 
 _CarType = Hashable
-_MoveType = Tuple[_CarType, _CarType]
+_MoveType = NamedTuple("_MoveType", [("car", "_CarType"), ("to", int)])
 
 
-class ParkingState:
+class _ParkingState:
     """Implements a wrapper of a list representing the state of a parking lot.
 
     Attributes:
@@ -38,14 +24,14 @@ class ParkingState:
         self._validate_state(input_list, empty_slot)
         self.cars = input_list
         self.symbol_empty = empty_slot
+        self._positions = {car: index for index, car in enumerate(self.cars)}
+        self._seen_states = set()
 
     def __len__(self):
         return len(self.cars)
 
-    def _swap(self, x_ind: int, y_ind: int = None):
-        """Swaps two elements of the list at the positions x_ind and y_ind."""
-        y_ind = y_ind or self._get_position_empty()
-        self.cars[x_ind], self.cars[y_ind] = self.cars[y_ind], self.cars[x_ind]
+    def __getitem__(self, idx):
+        return self.cars[idx]
 
     @staticmethod
     def _validate_state(state: List[_CarType], symbol_empty: _CarType):
@@ -74,30 +60,124 @@ class ParkingState:
         if len(state) != len(set(state)):
             raise ValueError("Invalid input: duplicate element(s) found.")
 
-    def _validate_two_states(self, state: "ParkingState"):
-        if len(self.cars) != len(state):
-            raise ValueError(
-                f"States' lengths mismatch, {len(self)} != {len(state)}")
-        if self.symbol_empty != state.symbol_empty:
-            raise ValueError(
-                f"The two states have different empty slot symbols: "
-                f"{self.symbol_empty} & {state.symbol_empty}.")
-        if set(self.get()) != set(state.get()):
-            raise ValueError(
-                "The two sets of cars are different. Cannot find moves.")
+    def _get_next_fewer_move(self, displaced_cars: Set[_CarType],
+                             target_state: "_ParkingState") -> Generator[
+        _MoveType, None, None]:
+        """Generates a sequence of moves from the start state to the end state.
 
-    def _find_diff(self, state: "ParkingState") -> Set[_CarType]:
-        """Returns elements of current state that differ from those of state."""
-        self._validate_two_states(state)
-        return {car for car, end_car in zip(self.cars, state.get())
-                if car != end_car and car != self.symbol_empty}
+        We can't decrease the count of incorrectly parked cars by more
+        than 1 for one movement.
+        If the empty slot is not in the right place we can decrease
+        count of incorrect cars by 1 (we can move correct car to empty place).
+        If initially the empty slot is in the right place we can neither
+        decrease not increase the count of incorrect cars
+        (we can move any incorrect car to empty place).
+        Number of times when the empty slot will be on its right place equals
+        to the number of cycles in the permutation.
+        So, minimal count of movements = SUM (l_i + 1), where i from 1 to N,
+        N is the count of permutation's cycles, l_i is a length of i-th cycle.
+
+        Yields:
+            A car move (car, target position).
+        """
+        while displaced_cars:
+            empty_position = self._positions[self.symbol_empty]
+            # the empty slot is in the right place
+            if empty_position == target_state._positions[self.symbol_empty]:
+                displaced_car = displaced_cars.pop()  # any displaced car
+                yield self._swap_cars_and_pos(self._positions[displaced_car],
+                                              self._positions[
+                                                  self.symbol_empty])
+                displaced_cars.add(displaced_car)
+            # in the place of the empty slot there should be another car
+            displaced_car = target_state[self._positions[self.symbol_empty]]
+            yield self._swap_cars_and_pos(self._positions[displaced_car],
+                                          self._positions[self.symbol_empty])
+            displaced_cars.discard(displaced_car)
+
+    def _get_next_constrained_move(self, current_moves: List[_MoveType],
+                                   target_state: "_ParkingState",
+                                   displaced_cars: Set[_CarType],
+                                   constraints: Dict[
+                                       int, Set[_CarType]] = None) -> Optional[
+        List[_MoveType]]:
+        """Finds a constrained path from the current state to the target one.
+
+        It tries to make all feasible steps from the current state and
+        backtracks if no possible moves left. It does not go to the previous
+        step and stores all visited configurations to avoid infinite loops.
+
+        Args:
+            current_moves: A list of moves currently being done.
+            target_state: The target arrangement of the cars.
+            displaced_cars: A set of cars that are not in their right positions.
+            constraints: A map of constraints telling which cars (values)
+            can be moved to which slots (keys).
+
+        Returns:
+            None if there is no solution,
+            otherwise a list of moves leading to the target state.
+        """
+        if not displaced_cars:
+            return current_moves
+
+        state = tuple(self.cars)
+        if state in self._seen_states:
+            return
+        self._seen_states.add(state)
+
+        empty_position = self._positions[self.symbol_empty]
+        feasible_cars = self._get_feasible_cars(constraints, empty_position)
+        for next_car in feasible_cars:
+            if current_moves and current_moves[-1].car == next_car:
+                continue
+            car_was_in_target = self._positions[next_car] == \
+                                target_state._positions[next_car]
+            car_moved_to_target = empty_position == target_state._positions[
+                next_car]
+            if car_was_in_target:
+                displaced_cars.add(next_car)
+            elif car_moved_to_target:
+                displaced_cars.remove(next_car)
+
+            move = self._swap_cars_and_pos(self._positions[next_car],
+                                           empty_position)
+            current_moves.append(move)
+            empty_position = self._positions[self.symbol_empty]
+            result = self._get_next_constrained_move(current_moves,
+                                                     target_state,
+                                                     displaced_cars,
+                                                     constraints)
+            if result is not None:
+                return result
+            else:
+                empty_position = self._restore_state(car_moved_to_target,
+                                                     car_was_in_target,
+                                                     current_moves,
+                                                     displaced_cars,
+                                                     empty_position,
+                                                     next_car)
+
+    def _restore_state(self, car_moved_to_target, car_was_in_target,
+                       current_moves, displaced_cars, empty_position,
+                       feasible_car) -> int:
+        """Restores the state when backtracking and returns the new empty slot."""
+        self._swap_cars_and_pos(self._positions[feasible_car],
+                                empty_position)
+        empty_position = self._positions[self.symbol_empty]
+        current_moves.pop()
+        if car_was_in_target:
+            displaced_cars.remove(feasible_car)
+        elif car_moved_to_target:
+            displaced_cars.add(feasible_car)
+        return empty_position
 
     def _get_feasible_cars(self, constraints: Dict[int, Set[_CarType]],
                            position_empty: int) -> Set[_CarType]:
         """Returns set of cars that can be moved to position_empty.
 
         Args:
-            constraints: A set of constraints, where a constraint is the fact
+            constraints: A map of constraints, where a constraint is the fact
             that a certain parking place is reserved only for certain cars.
             position_empty: Target position (e.g., that of the empty slot).
 
@@ -105,63 +185,17 @@ class ParkingState:
             A set of cars that can be moved to position_empty.
         """
         if constraints is not None and position_empty in constraints:
-            cars = set(constraints[position_empty])
+            return set(constraints[position_empty]) - {self.symbol_empty}
         else:
-            cars = set(self.get())
-        return cars - {self.symbol_empty}
+            return set(self.cars) - {self.symbol_empty}
 
-    def _get_next_move(self, target_state: "ParkingState",
-                       constraints: Dict[int, Set[_CarType]] = None):
-        """#todo: doc
-
-        Args:
-            target_state:
-            constraints:
-
-        Returns:
-
-        """
-        displaced_cars = self._find_diff(target_state)
-        logging.debug(("displaced cars: ", displaced_cars))
-        positions = self._get_positions()
-        target_empty_position = target_state._get_position_empty()
-        flag_random = False  # quick "fix"
-
-        while displaced_cars:
-            position_empty = positions[self.symbol_empty]
-            if flag_random:  # todo: remove this branch when fixed
-                car = self._get_feasible_cars(constraints,
-                                              position_empty).pop()
-                flag_random = False
-            elif position_empty == target_empty_position:
-                feasible_cars = self._get_feasible_cars(constraints,
-                                                        position_empty)
-                intersection = displaced_cars & feasible_cars
-                if not intersection:
-                    car = feasible_cars.pop()
-                    displaced_cars.add(car)
-                    flag_random = True
-                else:
-                    car = intersection.pop()
-            else:
-                car = target_state.get()[position_empty]
-                displaced_cars.discard(car)
-
-            self._swap(positions[car], position_empty)
-            positions[car], positions[self.symbol_empty] = (position_empty,
-                                                            positions[car])
-            logging.debug((car, " -> 0", positions[car], displaced_cars))
-            yield car, positions[car]
-
-    def get(self):
-        return self.cars
-
-    def _get_position_empty(self):
-        return self._get_positions()[self.symbol_empty] if len(
-            self) > 0 else None
-
-    def _get_positions(self):
-        return {x: index for index, x in enumerate(self.cars)}
+    def _swap_cars_and_pos(self, x_ind: int, y_ind: int):
+        """Swaps two elements at the given positions and returns that move."""
+        self.cars[x_ind], self.cars[y_ind] = self.cars[y_ind], self.cars[x_ind]
+        self._positions[self.cars[x_ind]], self._positions[self.cars[y_ind]] = (
+            self._positions[self.cars[y_ind]],
+            self._positions[self.cars[x_ind]])
+        return _MoveType(self.cars[y_ind], y_ind)
 
 
 class ParkingLot:
@@ -181,14 +215,60 @@ class ParkingLot:
     """
 
     def __init__(self, start: List[_CarType], empty: _CarType = 0,
-                 constraints: Dict[_CarType, Set[_CarType]] = None):
-        self.state = ParkingState(start, empty)
+                 constraints: Dict[int, Set[_CarType]] = None):
+        self.state = _ParkingState(start, empty)
         if constraints is not None:
             self._validate_constraints(constraints)
         self.constraints = constraints
 
     def __len__(self):
         return len(self.state)
+
+    def get_moves(self, target_state: List[_CarType],
+                  retain_state: bool = False) -> List[_MoveType]:
+        """Computes a sequence of moves from the start state to the target one.
+
+        Unless deselected, self.state is updated as the moves are generated,
+        and finally set to target_state.
+
+        Args:
+            target_state: Targeted state (arrangement of cars).
+            retain_state: Retains self.state unchanged if True,
+            updates it as moves are generated if False.
+
+        Returns:
+            List of car moves (car, position) where car is any _CarType object,
+            and the latter is the position to which car should be moved.
+        """
+        target_state = _ParkingState(target_state,
+                                     self.state.symbol_empty)
+        self._validate_two_states(target_state)
+        self._validate_feasibility(target_state)
+        displaced_cars = self._find_diff(target_state)
+        current_state = self.state if not retain_state else copy.deepcopy(
+            self.state)
+        if self.constraints is None:
+            return list(
+                current_state._get_next_fewer_move(displaced_cars,
+                                                   target_state))
+        else:
+            return current_state._get_next_constrained_move(
+                current_moves=[],
+                target_state=target_state,
+                displaced_cars=displaced_cars,
+                constraints=self.constraints)
+
+    def _validate_two_states(self, state: "_ParkingState"):
+        if len(self.state.cars) != len(state):
+            raise ValueError(
+                f"States' lengths mismatch, {len(self.state)} != {len(state)}")
+        if self.state.symbol_empty != state.symbol_empty:
+            raise ValueError(
+                f"The two states have different empty slot symbols: "
+                f"{self.state.symbol_empty} & {state.symbol_empty}.")
+        if set(self.state.cars) != set(state.cars):
+            raise ValueError(
+                "The two sets of cars are different. Cannot find moves.")
 
     def _validate_constraints(self, constraints: Dict[int, Set[_CarType]]):
         """Validates if constraints are applicable to the input.
@@ -219,48 +299,12 @@ class ParkingLot:
             if not 0 <= position <= len(self):
                 raise ValueError(
                     f"Out of bounds. {position} not in [0, {len(self)}]")
-            if len(cars & set(self.get_state())) != len(cars):
+            if len(cars & set(self.state.cars)) != len(cars):
                 raise ValueError("Unrecognized vehicle(s).")
 
             constraints[position].add(self.state.symbol_empty)
 
-    def get_moves(self, target_state: List[_CarType],
-                  retain_state: bool = False) -> List[_MoveType]:
-        """
-        Computes a sequence of moves from the start state to the given
-        target state. Unless deselected, self.state is updated as the
-        moves are generated, and finally set to target_state.
-        :param target_state: [list of _CarType objects] targeted state
-        :param retain_state: [bool] retains self.state unchanged if True,
-         updates it as moves are generated if False
-        :return: [list of tuples (car, int)] where car is any _CarType object,
-        and the latter is the position to which car should be moved
-        """
-        target_state = ParkingState(target_state,
-                                    self.state.symbol_empty)
-        self.state._validate_two_states(target_state)
-        self._validate_feasibility(target_state)
-
-        current_state = self.state if not retain_state else copy.deepcopy(
-            self.state)
-
-        logging.debug(("start: ", self.state.get()))
-        logging.debug(("end: ", target_state.get()))
-
-        return list(
-            current_state._get_next_move(target_state, self.constraints))
-
-    def get_state(self):
-        """Returns the current state of the parking lot."""
-        return self.state.get()
-
-    def update_constraints(self, constraints: Dict[int, Set[_CarType]]):
-        """Adds/updates constraints to the current parking state."""
-        if constraints is not None:
-            self._validate_constraints(constraints)
-        self.constraints = constraints
-
-    def _validate_feasibility(self, target_state: ParkingState):
+    def _validate_feasibility(self, target_state: _ParkingState):
         """Checks for contradiction between constraints and the target state.
 
         Args:
@@ -272,7 +316,19 @@ class ParkingLot:
         """
         if self.constraints is None:
             return
-        for pos, car in enumerate(target_state.get()):
+        for pos, car in enumerate(target_state.cars):
             if pos in self.constraints and car not in self.constraints[pos]:
                 raise ValueError(
                     "Found contradiction between constraints and target state.")
+
+    def _find_diff(self, state: "_ParkingState") -> Set[_CarType]:
+        """Returns elements of current state that differ from those of state."""
+        self._validate_two_states(state)
+        return {car for car, end_car in zip(self.state.cars, state.cars)
+                if car != end_car and car != self.state.symbol_empty}
+
+    def update_constraints(self, constraints: Dict[int, Set[_CarType]]):
+        """Adds/updates constraints to the current parking state."""
+        if constraints is not None:
+            self._validate_constraints(constraints)
+        self.constraints = constraints
--- group/assignment6/tests.py
+++ group/assignment6/tests.py
@@ -1,79 +1,34 @@
 #!/usr/bin/python3
 import unittest
 
-from code import ParkingLot, ParkingState
+from code import ParkingLot, _ParkingState
 
 
-class StateTest(unittest.TestCase):
+class ParkingStateTest(unittest.TestCase):
     def test_empty_state(self):
-        self.assertListEqual(ParkingState([], "").get(), [])
+        self.assertListEqual(_ParkingState([], "").cars, [])
 
     def test_basic_state(self):
         cars = [1, 2]
-        state = ParkingState(cars, 1)
-        self.assertListEqual(state.get(), cars)
+        state = _ParkingState(cars, 1)
+        self.assertListEqual(state.cars, cars)
 
     def test_invalid_empty_spot_symbol_not_present(self):
-        self.assertRaises(ValueError, ParkingState, [1, 2, 3], "a")
-        self.assertRaises(ValueError, ParkingState, [1, 2, 3], 4)
+        self.assertRaises(ValueError, _ParkingState, [1, 2, 3], "a")
+        self.assertRaises(ValueError, _ParkingState, [1, 2, 3], 4)
 
     def test_invalid_input_two_empty_slots(self):
-        self.assertRaises(ValueError, ParkingState, [1, 2, 0, 0], 0)
-
-    def test_consistent_states(self):
-        state1 = ParkingState([1, 2, 3], 1)
-        state2 = ParkingState([3, 2, 1], 1)
-        try:
-            state1._validate_two_states(state2)
-        except ValueError:
-            self.fail("Raised ValueError unexpectedly.")
-
-    def test_inconsistent_states_different_elements(self):
-        state1 = ParkingState([1, 2, 3], 1)
-        state2 = ParkingState([1, 2, 5], 1)
-        self.assertRaises(ValueError, state1._validate_two_states, state2)
-
-    def test_inconsistent_states_size_mismatch(self):
-        state1 = ParkingState([1, 2], 1)
-        state2 = ParkingState([1, 2, 3], 1)
-        self.assertRaises(ValueError, state1._validate_two_states, state2)
-
-    def test_inconsistent_states_different_empty_slot_symbol(self):
-        state1 = ParkingState([1, 2, 3], 1)
-        state2 = ParkingState([1, 2, 3], 2)
-        self.assertRaises(ValueError, state1._validate_two_states, state2)
-
-    def test_compare_identical(self):
-        state1 = ParkingState([1, 2, 3], 1)
-        state2 = ParkingState([1, 2, 3], 1)
-        self.assertEqual(state1._find_diff(state2), set([]))
-
-    def test_compare_different(self):
-        state1 = ParkingState([1, 2, 3], 1)
-        state2 = ParkingState([3, 1, 2], 1)
-        self.assertEqual(state1._find_diff(state2), {2, 3})
+        self.assertRaises(ValueError, _ParkingState, [1, 2, 0, 0], 0)
 
     def test_no_swap(self):
-        state1 = ParkingState([1, 2, 3], 1)
-        state1._swap(0)
-        self.assertEqual(state1.get(), [1, 2, 3])
+        state1 = _ParkingState([1, 2, 3], 1)
+        state1._swap_cars_and_pos(0, 0)
+        self.assertEqual(state1.cars, [1, 2, 3])
 
     def test_swap(self):
-        state1 = ParkingState([1, 2, 3], 1)
-        state1._swap(1)
-        self.assertEqual(state1.get(), [2, 1, 3])
-
-    def test_next_move_empty_slot_as_target(self):
-        state1 = ParkingState([1, 2, 3], 1)
-        state2 = ParkingState([1, 3, 2], 1)
-        next_move = state1._get_next_move(state2)
-        self.assertIn(next(next_move), [(2, 0), (3, 0)])
-
-    def test_next_move_correct_car(self):
-        state1 = ParkingState([1, 2, 3], 1)
-        state2 = ParkingState([2, 3, 1], 1)
-        next_move = state1._get_next_move(state2)
-        self.assertEqual(next(next_move), (2, 0))
+        state1 = _ParkingState([1, 2, 3], 1)
+        state1._swap_cars_and_pos(1, state1._positions[state1.symbol_empty])
+        self.assertEqual(state1.cars, [2, 1, 3])
 
 
 class ParkingLotTest(unittest.TestCase):
@@ -85,6 +40,53 @@ class ParkingLotTest(unittest.TestCase):
 
     def test_parking_size_2(self):
         self.assertListEqual(ParkingLot([0, 1]).get_moves([1, 0]), [(1, 0)])
+
+    def test_consistent_states(self):
+        state1 = ParkingLot([1, 2, 3], 1)
+        state2 = _ParkingState([3, 2, 1], 1)
+        try:
+            state1._validate_two_states(state2)
+        except ValueError:
+            self.fail("Raised ValueError unexpectedly.")
+
+    def test_inconsistent_states_different_elements(self):
+        state1 = ParkingLot([1, 2, 3], 1)
+        state2 = _ParkingState([1, 2, 5], 1)
+        self.assertRaises(ValueError, state1._validate_two_states, state2)
+
+    def test_inconsistent_states_size_mismatch(self):
+        state1 = ParkingLot([1, 2], 1)
+        state2 = _ParkingState([1, 2, 3], 1)
+        self.assertRaises(ValueError, state1._validate_two_states, state2)
+
+    def test_inconsistent_states_different_empty_slot_symbol(self):
+        state1 = ParkingLot([1, 2, 3], 1)
+        state2 = _ParkingState([1, 2, 3], 2)
+        self.assertRaises(ValueError, state1._validate_two_states, state2)
+
+    def test_compare_identical(self):
+        state1 = ParkingLot([1, 2, 3], 1)
+        state2 = _ParkingState([1, 2, 3], 1)
+        self.assertEqual(state1._find_diff(state2), set([]))
+
+    def test_compare_different(self):
+        state1 = ParkingLot([1, 2, 3], 1)
+        state2 = _ParkingState([3, 1, 2], 1)
+        self.assertEqual(state1._find_diff(state2), {2, 3})
+
+    def test_next_fewer_move_empty_slot_as_target(self):
+        lot1 = ParkingLot([1, 2, 3], 1)
+        state2 = _ParkingState([1, 3, 2], 1)
+        displaced_cars = lot1._find_diff(state2)
+        next_move = lot1.state._get_next_fewer_move(displaced_cars, state2)
+        self.assertIn(next(next_move), [(2, 0), (3, 0)])
+
+    def test_next_fewer_move_correct_car(self):
+        lot1 = ParkingLot([1, 2, 3], 1)
+        state2 = _ParkingState([2, 3, 1], 1)
+        displaced_cars = lot1._find_diff(state2)
+        next_move = lot1.state._get_next_fewer_move(displaced_cars, state2)
+        self.assertEqual(next(next_move), (2, 0))
 
     def test_parking_equal_start_end_states(self):
         cars = [0, 1, 2, 3]
@@ -99,13 +101,13 @@ class ParkingLotTest(unittest.TestCase):
         parking_lot = ParkingLot([1, 2, 0, 3])
         target_state = [3, 1, 2, 0]
         parking_lot.get_moves(target_state)
-        self.assertListEqual(parking_lot.get_state(), target_state)
+        self.assertListEqual(parking_lot.state.cars, target_state)
 
     def test_parking_state_retained(self):
         cars = [1, 2, 0, 3]
         parking_lot = ParkingLot(cars)
         parking_lot.get_moves([3, 1, 2, 0], retain_state=True)
-        self.assertListEqual(parking_lot.get_state(), cars)
+        self.assertListEqual(parking_lot.state.cars, cars)
 
     def test_constraint_type_error(self):
         constraints = [1, 2, 3]
@@ -126,7 +128,7 @@ class ParkingLotTest(unittest.TestCase):
 
     def test_parking_fewer_moves(self):
         cars = [1, 2, 0, 3]
-        parking_lot = ParkingLot(cars, 0)
+        parking_lot = ParkingLot(cars)
         final = [2, 3, 0, 1]
         self.assertIn(parking_lot.get_moves(final, retain_state=True),
                       [[(1, 2), (2, 0), (3, 1), (1, 3)],
@@ -136,8 +138,7 @@ class ParkingLotTest(unittest.TestCase):
         cars = [1, 2, 0, 3]
         parking_lot = ParkingLot(cars, 0)
         final = [2, 3, 0, 1]
-        # due to the following constraints,
-        # there could be one set of moves possible
+        # due to the following constraints, there can be 1 set of moves possible
         constraints = {2: {3}}
         parking_lot.update_constraints(constraints)
         self.assertListEqual(parking_lot.get_moves(final),
@@ -152,9 +153,8 @@ class ParkingLotTest(unittest.TestCase):
         self.assertListEqual(parking_lot.get_moves(final),
                              [(3, 2), (1, 3), (2, 0), (1, 1), (3, 3)])
 
-    @unittest.skip
     def test_parking_move_correctly_positioned_car_deeper(self):
-        _initial = [1, 2, 3, 4, 5, 0]
+        initial = [1, 2, 3, 4, 5, 0]
         final = [2, 1, 4, 5, 3, 0]
         constraints = {0: {1, 2},
                        1: {1, 2, 3},
@@ -162,11 +162,23 @@ class ParkingLotTest(unittest.TestCase):
                        3: {3, 4, 5},
                        4: {3, 4, 5},
                        5: {2}}
-        parking_lot = ParkingLot(_initial, 0, constraints)
+        parking_lot = ParkingLot(initial, 0, constraints)
         self.assertListEqual(parking_lot.get_moves(final),
-                             [(2, 5), (3, 1), (4, 2),
-                              (5, 3), (3, 4), (5, 3),
+                             [(2, 5), (3, 1), (4, 2), (5, 3),
                               (3, 4), (1, 1), (2, 0)])
+
+    def test_parking_constraints_with_no_solution(self):
+        # normally this configuration would be backtracked
+        initial = [0, 1, 3, 4, 5, 2]
+        final = [2, 1, 4, 5, 3, 0]
+        constraints = {0: {2},
+                       1: {1, 2, 3},
+                       2: {3, 4, 5},
+                       3: {3, 4, 5},
+                       4: {3, 4, 5},
+                       5: {2}}
+        parking_lot = ParkingLot(initial, 0, constraints)
+        self.assertIsNone(parking_lot.get_moves(final))
 
 
 if __name__ == "__main__":
